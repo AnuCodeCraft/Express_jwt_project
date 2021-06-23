daeaee3d027909bf9e94d487fb4e358a9efd85da
Anupriya Jha <anupriya.jha@medzgo.com>
2021-06-23 22:07:03 +0530

    updated

--- scheduler.js
+++ scheduler.js
@@ -2,13 +2,13 @@ const cron = require("node-cron");
 const mongoose = require("mongoose");
 const User = require('./models/User');
 
-// cron.schedule("*/5 * * * * ",() => {
-//     console.log("Running every minute");
-//     const user = User.deleteOne({},function(err, obj) {
-//         if (err) throw err;
-//         console.log("1 document deleted");
+cron.schedule("*/5 * * * * ",() => {
+    console.log("Running every minute");
+    const user = User.deleteOne({},function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
 
         
     
-// });
-// })
+});
+})
